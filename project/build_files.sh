8b30d09bdef9aebc51c34e58be9e53e34bd7cdac
Adwait Raghav <adwait.raghav@gmail.com>
2022-12-11 01:34:43 +0530

    dbsqlite removed

--- project/build_files.sh
+++ project/build_files.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+
 # Used by Vercel to build the application and deploy.
-echo "BUILD START"
+echo "BUILDING THE PROJECT"
 python3.6 -m pip install -r requirements.txt
+
+echo "MAKING MIGRATIONS"
+python3.6 manage.py makemigrations --noinput
+python3.6 manage.py migrate --noinput
+
+echo "COLLECT STATIC"
 python3.6 manage.py collectstatic --noinput --clear
+
 echo  "BUILD END"
