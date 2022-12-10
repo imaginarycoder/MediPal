2709dbd92172fb700877ce471e94dc8c261e5592
Adwait Raghav <adwait.raghav@gmail.com>
2022-12-11 02:46:15 +0530

    Testing postgres

--- project/build_files.sh
+++ project/build_files.sh
@@ -2,13 +2,13 @@
 
 # Used by Vercel to build the application and deploy.
 echo "BUILDING THE PROJECT"
-python3.6 -m pip install -r requirements.txt
+python3.6.8 -m pip install -r requirements.txt
 
 echo "MAKING MIGRATIONS"
 # python3.6 manage.py makemigrations --noinput
 # python3.6 manage.py migrate --noinput
 
 echo "COLLECT STATIC"
-python3.6 manage.py collectstatic --noinput --clear
+python3.6.8 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
