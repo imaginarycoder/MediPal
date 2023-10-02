a7b813320302307856e2f6f6425e435a3db41a6d
Adwait Raghav <adwait.raghav@gmail.com>
2023-10-02 19:34:58 +0530

    Added staticfiles for deployment

--- project/project/urls.py
+++ project/project/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('application/', include('application.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL , document_root = settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
