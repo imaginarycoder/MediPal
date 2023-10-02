eec3c67d87b50d21f392a2d427cb7f28bc5e22f4
Adwait Raghav <adwait.raghav@gmail.com>
2023-10-02 19:42:06 +0530

    urlpatters update

--- project/project/urls.py
+++ project/project/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('application/', include('application.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL , document_root = settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
