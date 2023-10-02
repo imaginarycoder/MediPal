4b4da25ee82977bcab8a92359b2eb1abe59a621a
Adwait Raghav <adwait.raghav@gmail.com>
2023-10-02 19:08:26 +0530

    url settings update

--- project/project/settings.py
+++ project/project/settings.py
@@ -17,9 +17,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
 PICKLE_DIR = os.path.join(BASE_DIR, 'pickle_files')
-MEDIA_URL = 'media/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
@@ -127,7 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
    STATIC_DIR
